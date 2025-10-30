a43363b3d6c25528479d9307d422c7a17c4d96b4
grizlx69 <ivanovff12333@gmail.com>
2025-10-30 17:02:57 +0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,42 @@
+console.log("Здравствуй мир!");
+
+function getFormValues() {
+    let form = document.getElementById('myForm');
+    let value_one = parseFloat(form.value_one.value);
+    let value_two = parseFloat(form.value_two.value);
+    return { value_one, value_two };
+}
+
+
+function addition(){
+    let values = getFormValues();
+    let result = values.value_one + values.value_two;
+    console.log(`${values.value_one} + ${values.value_two} = ${result}`);
+    showResult('+', values.value_one, values.value_two, result);
+}
+function substraction(){
+    let values = getFormValues();
+    let result = values.value_one - values.value_two;
+    console.log(`${values.value_one} - ${values.value_two} = ${result}`);
+    showResult('-', values.value_one, values.value_two, result);
+}
+
+function multiplication(){
+    let values = getFormValues();
+    let result = values.value_one * values.value_two;
+    console.log(`${values.value_one} * ${values.value_two} = ${result}`);
+    showResult('*', values.value_one, values.value_two, result);
+}
+
+function division(){
+    let values = getFormValues();
+    let result = values.value_one / values.value_two;
+    console.log(`${values.value_one} / ${values.value_two} = ${result}`);
+    showResult('/', values.value_one, values.value_two, result);
+}
+
+
+function showResult(operation, value_one, value_two, result) {
+    let output = document.getElementById('result');
+    output.textContent = `${value_one} ${operation} ${value_two} = ${result}`;
+}
